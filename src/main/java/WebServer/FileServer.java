1310966fca94a0ee1c9212746ab0e6b3fdbd6040
Putin Pavel <putin@cs.vsu.ru>
2022-09-12 10:14:44 +0300

    Сделан функционал отправки файла

--- src/main/java/WebServer/FileServer.java
+++ src/main/java/WebServer/FileServer.java
@@ -1,10 +1,14 @@
 package WebServer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,15 +32,46 @@ public class FileServer {
         try {
             System.out.println("Server started...");
             Socket client = server.accept();
+            System.out.println("Socket accepted...");
 
             Scanner input = new Scanner(client.getInputStream());
             String startingLine = input.nextLine();
+            System.out.println(startingLine);
+            Path url = Paths.get(getUrl(startingLine));
+            Path appName = url.getParent();
 
-            String url = getUrl(startingLine);
-            String appName = getAppNameFromURL(url);
-            String targetName = getFileNameFromURL(url);
+            System.out.println(appName);
 
+            if (!appName.equals(Paths.get("/app/hello"))) {
+                return;
+            }
 
+            Path targetName = url.getFileName();
+            Path target = configuration.getRoot().resolve(targetName);
+
+            if (!target.toFile().exists()) {
+                throw new FileNotFoundException("Target file not found");
+            }
+
+            if (!target.toFile().isFile()) {
+                throw new IllegalArgumentException("Target path doesn't lead to a file");
+            }
+
+            if (!target.toFile().canRead()) {
+                throw new RuntimeException("Target file isn't readable");
+            }
+
+            FileInputStream fis = new FileInputStream(target.toFile());
+            byte[] content = fis.readAllBytes();
+
+            String responce = "HTTP 200 OK\r\n" +
+                    "Content-length: " + content.length + "\r\n" +
+                    "Connection: close\r\n\r\n";
+
+            OutputStream os = client.getOutputStream();
+            os.write(responce.getBytes());
+            os.write(content);
+            os.flush();
 
             client.close();
         } catch (IOException e) {
